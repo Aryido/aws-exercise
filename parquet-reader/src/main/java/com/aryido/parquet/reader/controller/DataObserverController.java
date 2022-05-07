7459f5d9a2be18df2d7769b782b619eb28cd6ce6
Aryido_glb <f104882013_glb@gmail.com>
2022-05-08 00:04:02 +0800

    Debug: fix error return file path

--- parquet-reader/src/main/java/com/aryido/parquet/reader/controller/DataObserverController.java
+++ parquet-reader/src/main/java/com/aryido/parquet/reader/controller/DataObserverController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -13,7 +14,7 @@ import java.util.Optional;
 /**
  * @author YunYang Lee
  */
-//@RestController
+@RestController
 public class DataObserverController {
 	private final IS3Repository<KinesisData> repository;
 
